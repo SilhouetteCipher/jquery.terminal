aa4bc6e095812c678cc1b3bbee2357ac152d1721
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2023-08-10 13:49:05 +0100

    latest

--- script.js
+++ script.js
@@ -17,6 +17,31 @@ $(document).ready(function() {
             $.getJSON('https://silhouettecipher.github.io/jquery.terminal/narrative.json', function(data) {
                 term.echo(data.greetings, { raw: true });
             });
+        },
+        process: function(command) {
+            // Remove non-breaking spaces and return the cleaned command
+            return command.replace(/\u00A0/g, ' ');
         }
     });
 });
+
+$('#terminal').on('keypress', function(e) {
+    if (e.which === 160) { // 160 is the char code for non-breaking space
+        e.preventDefault(); // Prevent the non-breaking space from being added
+        const currentInput = terminal.get_command();
+        terminal.set_command(currentInput + ' '); // Add a regular space instead
+    }
+});
+//codepen crt
+$(document).ready(function() {
+    const terminal = $('#terminal');
+    
+    function adjustBackgroundSize() {
+        const size = (Math.random() * 50) + 50 + '% ' + (Math.random() * 50) + 50 + '%';
+        terminal.css('--size', size);
+    }
+
+    setInterval(adjustBackgroundSize, 100); // Reduced interval for more frequent flickering
+});
+
+
